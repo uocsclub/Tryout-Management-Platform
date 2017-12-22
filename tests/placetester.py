f5252757d34388bcb8af6875c8a22f34efbb09c4
Rodger Waldron <rodger.waldron@gmail.com>
2017-12-22 01:31:36 -0500

    added placetester

---
+++ tests/placetester.py
@@ -0,0 +1,13 @@
+#Is a place holder tests
+import unittest
+
+
+class PlaceTester(unittest.TestCase):
+    """Best place tester """
+
+    def test_absolute_truth_and_meaning(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
