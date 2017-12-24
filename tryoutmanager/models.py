ea43742d267e93ba72da5a6c3e82e632ea787e36
verandaguy <julesmazur@gmail.com>
2017-12-24 11:02:43 -0500

    Misc fixes

--- tryoutmanager/models.py
+++ tryoutmanager/models.py
@@ -6,7 +6,7 @@ import sqlalchemy as sa
 
 from datetime import datetime
 from sqlalchemy import Column, String, Boolean, DateTime, Table, \
-                       ForeignKey, Enum, Index, UniqueConstraint
+                       ForeignKey, Enum, Index
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
@@ -20,15 +20,22 @@ Session = sessionmaker(bind=db)
 
 # Association tables for many-to-many relationships
 markers_challenges = Table(
-        'markers_challenges', Base.metadata, Column(
-            'marker_email', String, ForeignKey('markers.email')),
+        'markers_challenges',
+        Base.metadata,
+        Column('marker_email', String, ForeignKey('markers.email')),
         Column('challenge_id', UUID, ForeignKey('challenges.id')))
 
 challenges_submissions = Table(
-        'challenges_submissions', Base.metadata, Column(
-                                   'challenge_id', UUID,
-                                   ForeignKey('challenges.id')),
-        Column('submission_id', UUID, ForeignKey('submissions.id')))
+        'challenges_submissions',
+        Base.metadata,
+        Column(
+            'challenge_id',
+            UUID,
+            ForeignKey('challenges.id')),
+        Column(
+            'submission_id',
+            UUID,
+            ForeignKey('submissions.id')))
 
 
 # Tables corresponding to concrete models
@@ -37,8 +44,8 @@ class AdminUser(Base):
 
     email = Column(String, nullable=False, unique=True, primary_key=True)
     password = Column(String, nullable=False)
-    github_slug = Column(String, nullable=True)
-    public_keys = relationship('PublicKey', back_populates='user')
+    github_slug = Column(String)
+    public_keys = relationship('AdminUserPublicKey', back_populates='user')
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
 
 
@@ -49,10 +56,11 @@ class Marker(Base):
     password = Column(String, nullable=False)
     name = Column(String, nullable=False)
     challenges = relationship(
-            'Challenges', secondary=markers_challenges,
+            'Challenges',
+            secondary=markers_challenges,
             back_populates='markers')
-    github_slug = Column(String, nullable=True)
-    public_keys = relationship('PublicKey', back_populates='user')
+    github_slug = Column(String)
+    public_keys = relationship('MarkerPublicKey', back_populates='user')
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
 
 
@@ -63,29 +71,54 @@ class User(Base):
     password = Column(String, nullable=False)
     name = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
-    github_slug = Column(String, nullable=True)
-    public_keys = relationship('PublicKey', back_populates='user')
+    github_slug = Column(String)
+    public_keys = relationship('UserPublicKey', back_populates='user')
     confirmed = Column(Boolean, nullable=False, default=False)
     email_confirmation_slug = Column(String, unique=True)
     email_confirmation_expiry = Column(DateTime(timezone=True))
 
 
-class PublicKey(Base):
-    __tablename__ = 'public_keys'
+class UserKey(Base):
+    __tablename__ = 'user_keys'
 
     key = Column(String, nullable=False, primary_key=True)
+    owner = relationship('User', back_populates='public_keys')
+    owner_email = Column(String, ForeignKey('users.email'), nullable=False)
+
+
+class MarkerKey(Base):
+    __tablename__ = 'marker_keys'
+
+    key = Column(String, nullable=False, primary_key=True)
+    owner = relationship('Marker', back_populates='public_keys')
+    owner_email = Column(String, ForeignKey('markers.email'), nullable=False)
+
+
+class AdminUserKey(Base):
+    __tablename__ = 'admin_user_keys'
+
+    key = Column(String, nullable=False, primary_key=True)
+    owner = relationship('AdminUser', back_populates='public_keys')
+    owner_email = Column(
+                    String,
+                    ForeignKey('admin_users.email'),
+                    nullable=False)
 
 
 class Challenge(Base):
     __tablename__ = 'challenges'
 
     id = Column(UUID, default=uuid.uuid4, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    repository = Column(String, nullable=False, unique=True)
     markers = relationship(
-            'Markers', secondary=markers_challenges,
+            'Marker',
+            secondary=markers_challenges,
             back_populates='challenges')
     submissions = relationship(
-            'Submission', secondary=challenges_submissions,
+            'Submission',
+            secondary=challenges_submissions,
             back_populates='submissions')
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
 
@@ -101,8 +134,11 @@ class Submission(Base):
     challenge_id = Column(UUID, ForeignKey('challenges.id'), nullable=False)
     reservation = relationship('Reservation')
     status = Column(
-            Enum('in_progress', 'late', 'submitted',
-                 name='submission_status'),
+            Enum(
+                'in_progress',
+                'late',
+                'submitted',
+                name='submission_status'),
             default='closed', nullable=False)
 
     # A contestant may only have one submission per challenge
@@ -125,7 +161,11 @@ class Reservation(Base):
     cancelled = Column(Boolean, default=False, nullable=False)
     cancelled_at = Column(DateTime(timezone=True), nullable=True)
     status = Column(
-            Enum('open', 'closed', 'cancelled', name='reservation_status'),
+            Enum(
+                'open',
+                'closed',
+                'cancelled',
+                name='reservation_status'),
             default='closed', nullable=False)
     created_at = Column(
             DateTime(timezone=True), nullable=False, default=datetime.utcnow)
