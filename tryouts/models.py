e0c1a9ab03096b7c13b89d60ab4083960502dd63
t0xicCode <xavier.l@magnax.ca>
2019-02-18 11:02:43 -0500

    Added basic models

--- tryouts/models.py
+++ tryouts/models.py
@@ -1,3 +1,25 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Year(models.Model):
+    year = models.IntegerField(blank=False, null=False, unique=True, primary_key=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+
+class Challenge(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    active = models.BooleanField(default=False, null=False)
+
+
+class Attempt(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    grade = models.IntegerField(null=True, default=None)
+    started = models.DateTimeField()
+    ended = models.DateTimeField(null=True, default=None, db_index=True)
